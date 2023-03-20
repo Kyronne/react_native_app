63a4dbc23789cb5de15c9fb89be77fbed5cb7906
Kyronne <kyronne.parkes@gmail.com>
2023-03-20 11:48:47 +0000

    fixed import directs index.js

--- app/index.js
+++ app/index.js
@@ -1,16 +1,20 @@
 import { useState } from "react";
 import { View, ScrollView, SafeAreaView } from 'react-native';
 import { Stack, useRouter } from 'expo-router';
-
-import { COLORS, icons, images, SIZES } from '../constants';
-import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components';
+import { COLORS, icons, images, SIZES } from '../constants/index.js';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components/index.js';
 
 const Home = () => {
     const router = useRouter();
 
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite}}>
-            <Stack.Screen></Stack.Screen>
+            <Stack.Screen 
+            options={{ 
+                headerStyle: { backgroundColor: COLORS.lightWhite }
+            }}>
+
+            </Stack.Screen>
         </SafeAreaView>
     )
 }
